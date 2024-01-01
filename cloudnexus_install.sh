baffe4f4d20b5f9dc5294a3fc089e0dfe5976f84
unknown <ch.hammad.khaliq@gmail.com>
2024-01-02 00:29:34 +0500

    Update Code

--- cloudnexus_install.sh
+++ cloudnexus_install.sh
@@ -89,7 +89,7 @@ echo "Checking if any services should be monitored..."
 if [ "$4" != "0" ]
 then
 	echo "Services found, inserting them into the agent config..."
-	sed -i "s/CheckServices=\"\"/CheckServices=\"$3\"/" /etc/cloudnexus/cloudnexus.cfg
+	sed -i "s/CheckServices=\"\"/CheckServices=\"$4\"/" /etc/cloudnexus/cloudnexus.cfg
 fi
 echo "... done."
 
