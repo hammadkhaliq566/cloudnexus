0523200f508862db37a3b67fbd58a2769ed65dcf
unknown <ch.hammad.khaliq@gmail.com>
2024-01-02 23:07:11 +0500

    POST Test

--- cloudnexus_install.sh
+++ cloudnexus_install.sh
@@ -184,7 +184,7 @@ else
 fi
 
 # Create JSON response
-json_response='{"status":"'"$status_code"'","UID":"'"$User_ID"'"}'
+json_response='{"status":"'"$status_code"'","SID":"'"$SID"'"}'
 
 # Print JSON response
 echo "$json_response"
