199dd703329132d123df24d78a1968a6e1c46e4e
unknown <ch.hammad.khaliq@gmail.com>
2024-01-05 00:05:05 +0500

    POST Test

--- cloudnexus_agent.sh
+++ cloudnexus_agent.sh
@@ -538,7 +538,7 @@ Filename="cloudnexus_agent_$Date_$Time.log"
 echo "$json" > "$ScriptPath"/"$Filename"
 
 # Post data
-wget --retry-connrefused --waitretry=1 -t 3 -T 15 -qO- --header="Content-Type: text/plain" --post-data="$json" https://6899-2400-adc5-154-f400-71ee-5ff4-8114-a550.ngrok-free.app/api/user/addServer/ &> /dev/null
+wget --retry-connrefused --waitretry=1 -t 3 -T 15 -qO- --header="Content-Type: text/plain" --post-data="$json" https://6210-2400-adc5-154-f400-c09-f75f-f3ce-6ac0.ngrok-free.app/api/user/addServer/ &> /dev/null
 
 if [ $? -eq 0 ]; then
     echo "Data posted successfully!"
